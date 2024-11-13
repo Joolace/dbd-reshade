05b47c9e23ad7ee8f7204eedece029ccd33b3dd9
E21GL98 <gleminem1@gmail.com>
2024-11-13 12:12:15 +0100

    Update OPEN DBD RESHADE.bat

--- OPEN DBD RESHADE.bat
+++ OPEN DBD RESHADE.bat
@@ -45,7 +45,7 @@ echo                                    IHHHHHHHIIJJJJKKKKLLLLMMMMNNQ
 echo                                        KJJIIIJJJKKKKLLLLMNO                                        
 echo.
 echo ================================
-echo Version: 1.1.2
+echo Version: 1.1.3
 echo ================================
 echo.
 powershell.exe -ExecutionPolicy Bypass -File "%~dp0dbdreshade.ps1"
